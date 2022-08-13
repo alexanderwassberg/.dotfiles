d999b7ce8c13d2f30eaec8e6efffdbbfacaea222
Alexander Wassberg <alexander.wassberg@askas.se>
2022-08-13 20:16:35 +0200

    alias - clear && cd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,6 +9,7 @@ alias ls "exa --grid --icons"
 alias lg "exa --long --tree --level=2 --git --icons"
 alias g "gitui"
 alias weather "curl wttr.in"
+alias cl "clear && cd"
 
 # Autoloads Starship Prompt
 starship init fish | source
