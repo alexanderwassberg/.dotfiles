caa83f2eb034edbc7eae5b4d683d3bd66024a1b9
Alexander Wassberg <alexander.wassberg@askas.se>
2022-08-16 15:21:01 +0200

    Fish shortcuts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,22 @@
+# No fish_greeting
 set fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Autoloads Starship Prompt
+starship init fish | source
+
+# Exa & Listings
 alias ll "exa --long --icons"
 alias ls "exa --grid --icons"
 alias lg "exa --long --tree --level=2 --git --icons"
-alias g "gitui"
+
+# Git
+alias g "git"
+alias gu "gitui"
+
+# Additionals
 alias weather "curl wttr.in"
 alias cl "clear && cd"
-
-# Autoloads Starship Prompt
-starship init fish | source
