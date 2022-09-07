5c46585bd1e00a4a74e2413509d6dd1a8b986f19
Alexander Wassberg <alexander.wassberg@askas.se>
2022-09-07 09:32:29 +0200

    formatting../..

--- nvim/.config/nvim/after/plugin/feline-nvim.rc.lua
+++ nvim/.config/nvim/after/plugin/feline-nvim.rc.lua
@@ -1,2 +1,4 @@
 local ctp_feline = require('catppuccin.groups.integrations.feline')
-require("feline").setup({ components = ctp_feline.get() })
+require("feline").setup({
+  components = ctp_feline.get()
+})
