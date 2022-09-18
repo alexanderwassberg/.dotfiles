b6f420078496c6539e3b72ea3888c964d16aa689
Alexander Wassberg <alexander.wassberg@askas.se>
2022-09-18 09:32:09 +0200

    dashboard

--- nvim/.config/nvim/after/plugin/dashboard.rc.lua
+++ nvim/.config/nvim/after/plugin/dashboard.rc.lua
@@ -17,14 +17,3 @@ db.custom_header = {
 }
 db.preview_file_height = 12
 db.preview_file_width = 80
-db.custom_center = {
-  { icon = '  ',
-    desc = 'Find  File                              ',
-    action = 'Telescope find_files find_command=rg,--hidden,--files' },
-  { icon = '  ',
-    desc = 'File Browser                            ',
-    action = 'Telescope file_browser' },
-  { icon = '  ',
-    desc = 'Find  word                              ',
-    action = 'Telescope live_grep' },
-}
